9e30883488208f08f2938633d453197de2c11131
pk1890 <piotrekkotara@gmail.com>
2019-10-21 14:51:42 +0200

    Changed synchronized stack to semaphores

--- src/lab2/Shop.java
+++ src/lab2/Shop.java
@@ -4,11 +4,13 @@ import java.util.Stack;
 
 public class Shop {
     private CountingSemaphore carts;
+    private BinarySemaphore stack_sem;
 
     private Stack<Basket> baskets;
 
     public Shop(int cartNo){
         carts = new CountingSemaphore(cartNo);
+        stack_sem = new BinarySemaphore();
         baskets = new Stack<Basket>();
         for(int i = 0; i < cartNo; i++){
             baskets.push(new Basket(i));
@@ -18,12 +20,16 @@ public class Shop {
         carts.P();
         System.out.println("[SHOP] : basket gived. Free_baskets:" + carts.getValue());
         Basket basketToGive;
-        synchronized (this){basketToGive = baskets.pop();}
+        stack_sem.P();
+        basketToGive = baskets.pop();
+        stack_sem.V();
         return basketToGive;
     }
     public void returnBasket(Basket basket){
         carts.V();
-        synchronized (this) {baskets.push(basket);}
+        stack_sem.P();
+        baskets.push(basket);
+        stack_sem.V();
         System.out.println("[SHOP] : basket returned. Free_baskets:" + carts.getValue());
     }
 
